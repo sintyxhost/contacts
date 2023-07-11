6ac0c19ce61757b179e64325fb1fcfed17a7a105
R4 Club <74131129+Nikolai980@users.noreply.github.com>
2023-07-11 16:45:46 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+<html>
+  <head>
+    <title>Contact Us</title>
+    <style>
+      body {
+        background-image: url(background.jpeg);
+        background-position: center;
+        background-repeat: no-repeat;
+        background-size: cover;
+        color: white;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+      }
+      .contact-container {
+        display: flex;
+        align-items: center;
+      }
+      .logo {
+        margin-left: 10px;
+        width: 540px;
+        height: 540px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="contact-container">
+      <div>
+        <h1>Contact Us</h1>
+        <p>Phone: +1 2125409644</p>
+        <p>Email: administrator@sintyx.eu.org</p>
+        <p>Discord: R4#0710</p>
+        <p>Address: 13820 NE Airport Way, Suite #K464848, Portland, OR 97251-1158, United States</p>
+        <p> Please note that the phone support line is only available in English/Bulgarian!!! Have a great day!</p>
+      </div>
+      <img class="logo" src="logo.png" alt="Company Logo">
+    </div>
+  </body>
+</html>
